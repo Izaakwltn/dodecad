7cb99bf60a81bb48e5cf3e92c38a526a0d773284
izaak <izaakw@protonmail.com>
2025-11-07 14:03:18 -0800

    Matrix only uses raw operations

--- src/dodecad.lisp
+++ src/dodecad.lisp
@@ -11,26 +11,15 @@
   '(0 1 2 3 4 5 6 7 8 9 10 11))
 
 ;;;
-;;; Turning tone row lists '(0 1 2 3 4 5 6 7 8 9 10 11) into 64 bit integer representation
-;;;
-;;; <1-bit Inverse>|<1-bit Retrograde>|<4 bits root><8 bits free>|<48 bits tone-row>
+;;; Turning tone row lists '(0 1 2 3 4 5 6 7 8 9 10 11) into 48 bit integer representation
 ;;;
 
-(defun %encode-prefix (root inverse retrograde)
-  (ash (logior (if inverse    #b10000000 #b0)
-	       (if retrograde #b01000000 #b0)
-	       root)
-       48))
-
-(defun %encode-row (tones &key inverse retrograde)
+(defun %encode-row (tones)
   "Encode a list of tones"
   (loop :with encoded := 0
 	:for tone :in (reverse tones)
 	:do (setq encoded (+ tone (ash encoded 4)))
-	:finally (return (logior (%encode-prefix (first tones)
-						 inverse
-						 retrograde)
-				 encoded))))
+	:finally (return encoded)))
 
 (defun %decode-row (row)
   (loop :with decoding := row
@@ -40,63 +29,63 @@
 	:finally (return decoded)))
 
 ;;;
-;;; Prefix manipulation
-;;;
-
-(defun %flip-flag (row index)
-  (dpb (mod (1+ (ldb (byte 1 index) row)) 2)
-       (byte 1 index)
-       row))
-
-(defun %flip-inverse-flag (row)
-  (%flip-flag row 63))
-
-(defun %flip-retrograde-flag (row)
-  (%flip-flag row 62))
-
-;;;
 ;;; Encoded Row operations
 ;;;
 
-(defmacro %with-transform (row &body body)
+(defmacro %with-transform (out &body body)
   "Transform a row according to a function applied to "
-  `(let ((prefix (logand #xFFFF000000000000 ,row))
-	 (tones  (logand #x0000FFFFFFFFFFFF ,row))
-	 (out    #x0000000000000000))
-     (loop :for i :from 0 :to 44 :by 4
-	   :do ,@body
-	   :finally (return (logior prefix out)))))
+  `(loop :with out := ,out
+	 :for i :from 0 :to 44 :by 4
+	 :do (setq out ,@body)
+	 :finally (return out)))
 
-(defun %transpose (row degree)
-  (%with-transform row
-    (setq out (logior (ash (mod (+ degree
-				   (logand #xF (ash tones (- i))))
-					  12)
-				     i)
-				out))))
+(defun %transpose-raw (row degree)
+  (%with-transform #x0000000000000000
+    (logior (ash (mod (+ degree
+			   (logand #xF (ash row (- i))))
+			12)
+		   i)
+	      out)))
+
+(defun %transpose-to-root (row root)
+  (%transpose-raw row (- root (ldb (byte 4 0) row))))
 
 (defun transpose (row degree)
-  (%decode-row (%transpose (%encode-row row) degree)))
+  (%decode-row (%transpose-raw (%encode-row row) degree)))
 
-(defun %retrograde (row)
-  (%with-transform row
-    (setq out (logior (ash (logand #xF (ash tones (- i)))
+(defun %retrograde-raw (row)
+  (%with-transform #x0000000000000000
+    (setq out (logior (ash (logand #xF (ash row (- i)))
 			   (- 44 i))
 		      out))))
 
 (defun retrograde (row)
-  (%decode-row (%retrograde (%encode-row row))))
+  (%decode-row (%retrograde-raw (%encode-row row))))
 
-(defun %inverse (row)
-  (%with-transform row
-    (setq out (logior (ash (mod (- #b1100
-				   (logand #xF (ash tones (- i))))
-				12)
-			   i)
-		      out))))
+(defun %inverse-raw (row)
+  (let* ((p0 (ldb (byte 4 0) row))
+	 (offset (abs (- p0 (- 12 p0)))))
+    (%with-transform #x0000000000000000
+      (setq out (logior (ash (mod
+			      (+ offset
+				 (mod (- #b1100
+					 (logand #xF (ash row (- i))))
+				      12))
+			      12)
+			     i)
+			out)))))
 
 (defun inverse (row)
-  (%decode-row (%inverse (%encode-row row))))
+  (%decode-row (%inverse-raw (%encode-row row))))
+
+(defun %matrix-raw (row)
+  (let ((inverse (%inverse-raw row)))
+    (%with-transform nil
+      (append out (list (%transpose-to-root row (logand #xF (ash inverse (- i)))))))))
+
+(defun matrix (row)
+  (mapcar #'%decode-row
+	  (%matrix-raw (%encode-row row))))
 
 ;;;
 ;;; Printing rows and matrices
@@ -118,6 +107,5 @@
 
 (defun print-matrix (row &optional (stream *standard-output*))
   "Generate and print the tone row matrix for the given row."
-  (let* ((p0 (first row)))
-    (loop :for root :in (inverse row)
-	  :do (%print-row (transpose row (mod (+ p0 root) 12)) stream))))
+  (dolist (row (matrix row))
+      (%print-row row stream)))
